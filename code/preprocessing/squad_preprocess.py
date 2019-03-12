b11f134e9309a18fa41caac4fbaaf7f8230837bf
siddharth bhavsar <siddharth.bhavsar@marutitech.com>
2019-03-12 18:44:33 +0530

    quick commit

--- code/preprocessing/squad_preprocess.py
+++ code/preprocessing/squad_preprocess.py
@@ -23,9 +23,9 @@ import nltk
 import numpy as np
 from tqdm import tqdm
 from six.moves.urllib.request import urlretrieve
+from importlib import reload
 
 reload(sys)
-sys.setdefaultencoding('utf8')
 random.seed(42)
 np.random.seed(42)
 
@@ -39,7 +39,7 @@ def setup_args():
 
 
 def write_to_file(out_file, line):
-    out_file.write(line.encode('utf8') + '\n')
+    out_file.write(line.encode('utf8') + '\n'.encode('ascii'))
 
 
 def data_from_json(filename):
@@ -94,22 +94,22 @@ def maybe_download(url, filename, prefix, num_bytes=None):
     local_filename = None
     if not os.path.exists(os.path.join(prefix, filename)):
         try:
-            print "Downloading file {}...".format(url + filename)
+            print("Downloading file {}...".format(url + filename))
             with tqdm(unit='B', unit_scale=True, miniters=1, desc=filename) as t:
-                local_filename, _ = urlretrieve(url + filename, os.path.join(prefix, filename), reporthook=reporthook(t))
+                local_filename, _ = urlretrieve(url + filename, os.path.join(prefix, filename),
+                                                reporthook=reporthook(t))
         except AttributeError as e:
-            print "An error occurred when downloading the file! Please get the dataset using a browser."
+            print("An error occurred when downloading the file! Please get the dataset using a browser.")
             raise e
     # We have a downloaded file
     # Check the stats and make sure they are ok
     file_stats = os.stat(os.path.join(prefix, filename))
     if num_bytes is None or file_stats.st_size == num_bytes:
-        print "File {} successfully loaded".format(filename)
+        print("File {} successfully loaded".format(filename))
     else:
         raise Exception("Unexpected dataset size. Please get the dataset using a browser.")
 
     return local_filename
-
 
 
 def get_char_word_loc_mapping(context, context_tokens):
@@ -127,19 +127,19 @@ def get_char_word_loc_mapping(context, context_tokens):
         e.g. if context = "hello world" and context_tokens = ["hello", "world"] then
         0,1,2,3,4 are mapped to ("hello", 0) and 6,7,8,9,10 are mapped to ("world", 1)
     """
-    acc = '' # accumulator
-    current_token_idx = 0 # current word loc
+    acc = ''  # accumulator
+    current_token_idx = 0  # current word loc
     mapping = dict()
 
-    for char_idx, char in enumerate(context): # step through original characters
-        if char != u' ' and char != u'\n': # if it's not a space:
-            acc += char # add to accumulator
-            context_token = unicode(context_tokens[current_token_idx]) # current word token
-            if acc == context_token: # if the accumulator now matches the current word token
-                syn_start = char_idx - len(acc) + 1 # char loc of the start of this word
-                for char_loc in range(syn_start, char_idx+1):
-                    mapping[char_loc] = (acc, current_token_idx) # add to mapping
-                acc = '' # reset accumulator
+    for char_idx, char in enumerate(context):  # step through original characters
+        if char != u' ' and char != u'\n':  # if it's not a space:
+            acc += char  # add to accumulator
+            context_token = str(context_tokens[current_token_idx])  # current word token
+            if acc == context_token:  # if the accumulator now matches the current word token
+                syn_start = char_idx - len(acc) + 1  # char loc of the start of this word
+                for char_loc in range(syn_start, char_idx + 1):
+                    mapping[char_loc] = (acc, current_token_idx)  # add to mapping
+                acc = ''  # reset accumulator
                 current_token_idx += 1
 
     if current_token_idx != len(context_tokens):
@@ -165,7 +165,7 @@ def preprocess_and_write(dataset, tier, out_dir):
       the number of (context, question, answer) triples written to file by the dataset.
     """
 
-    num_exs = 0 # number of examples written to file
+    num_exs = 0  # number of examples written to file
     num_mappingprob, num_tokenprob, num_spanalignprob = 0, 0, 0
     examples = []
 
@@ -174,46 +174,47 @@ def preprocess_and_write(dataset, tier, out_dir):
         article_paragraphs = dataset['data'][articles_id]['paragraphs']
         for pid in range(len(article_paragraphs)):
 
-            context = unicode(article_paragraphs[pid]['context']) # string
+            context = str(article_paragraphs[pid]['context'])  # string
 
             # The following replacements are suggested in the paper
             # BidAF (Seo et al., 2016)
             context = context.replace("''", '" ')
             context = context.replace("``", '" ')
 
-            context_tokens = tokenize(context) # list of strings (lowercase)
+            context_tokens = tokenize(context)  # list of strings (lowercase)
             context = context.lower()
 
-            qas = article_paragraphs[pid]['qas'] # list of questions
+            qas = article_paragraphs[pid]['qas']  # list of questions
 
-            charloc2wordloc = get_char_word_loc_mapping(context, context_tokens) # charloc2wordloc maps the character location (int) of a context token to a pair giving (word (string), word loc (int)) of that token
+            charloc2wordloc = get_char_word_loc_mapping(context,
+                                                        context_tokens)  # charloc2wordloc maps the character location (int) of a context token to a pair giving (word (string), word loc (int)) of that token
 
-            if charloc2wordloc is None: # there was a problem
+            if charloc2wordloc is None:  # there was a problem
                 num_mappingprob += len(qas)
-                continue # skip this context example
+                continue  # skip this context example
 
             # for each question, process the question and answer and write to file
             for qn in qas:
 
                 # read the question text and tokenize
-                question = unicode(qn['question']) # string
-                question_tokens = tokenize(question) # list of strings
+                question = str(qn['question'])  # string
+                question_tokens = tokenize(question)  # list of strings
 
                 # of the three answers, just take the first
-                ans_text = unicode(qn['answers'][0]['text']).lower() # get the answer text
-                ans_start_charloc = qn['answers'][0]['answer_start'] # answer start loc (character count)
-                ans_end_charloc = ans_start_charloc + len(ans_text) # answer end loc (character count) (exclusive)
+                ans_text = str(qn['answers'][0]['text']).lower()  # get the answer text
+                ans_start_charloc = qn['answers'][0]['answer_start']  # answer start loc (character count)
+                ans_end_charloc = ans_start_charloc + len(ans_text)  # answer end loc (character count) (exclusive)
 
                 # Check that the provided character spans match the provided answer text
                 if context[ans_start_charloc:ans_end_charloc] != ans_text:
-                  # Sometimes this is misaligned, mostly because "narrow builds" of Python 2 interpret certain Unicode characters to have length 2 https://stackoverflow.com/questions/29109944/python-returns-length-of-2-for-single-unicode-character-string
-                  # We should upgrade to Python 3 next year!
-                  num_spanalignprob += 1
-                  continue
+                    # Sometimes this is misaligned, mostly because "narrow builds" of Python 2 interpret certain Unicode characters to have length 2 https://stackoverflow.com/questions/29109944/python-returns-length-of-2-for-single-unicode-character-string
+                    # We should upgrade to Python 3 next year!
+                    num_spanalignprob += 1
+                    continue
 
                 # get word locs for answer start and end (inclusive)
-                ans_start_wordloc = charloc2wordloc[ans_start_charloc][1] # answer start word loc
-                ans_end_wordloc = charloc2wordloc[ans_end_charloc-1][1] # answer end word loc
+                ans_start_wordloc = charloc2wordloc[ans_start_charloc][1]  # answer start word loc
+                ans_end_wordloc = charloc2wordloc[ans_end_charloc - 1][1]  # answer end word loc
                 assert ans_start_wordloc <= ans_end_wordloc
 
                 # Check retrieved answer tokens match the provided answer text.
@@ -221,28 +222,35 @@ def preprocess_and_write(dataset, tier, out_dir):
                 # and the answer character span is around "generation",
                 # but the tokenizer regards "fifth-generation" as a single token.
                 # Then ans_tokens has "fifth-generation" but the ans_text is "generation", which doesn't match.
-                ans_tokens = context_tokens[ans_start_wordloc:ans_end_wordloc+1]
+                ans_tokens = context_tokens[ans_start_wordloc:ans_end_wordloc + 1]
                 if "".join(ans_tokens) != "".join(ans_text.split()):
                     num_tokenprob += 1
-                    continue # skip this question/answer pair
+                    continue  # skip this question/answer pair
 
-                examples.append((' '.join(context_tokens), ' '.join(question_tokens), ' '.join(ans_tokens), ' '.join([str(ans_start_wordloc), str(ans_end_wordloc)])))
+                examples.append((' '.join(context_tokens), ' '.join(question_tokens), ' '.join(ans_tokens),
+                                 ' '.join([str(ans_start_wordloc), str(ans_end_wordloc)])))
 
                 num_exs += 1
 
-    print "Number of (context, question, answer) triples discarded due to char -> token mapping problems: ", num_mappingprob
-    print "Number of (context, question, answer) triples discarded because character-based answer span is unaligned with tokenization: ", num_tokenprob
-    print "Number of (context, question, answer) triples discarded due character span alignment problems (usually Unicode problems): ", num_spanalignprob
-    print "Processed %i examples of total %i\n" % (num_exs, num_exs + num_mappingprob + num_tokenprob + num_spanalignprob)
+    print("Number of (context, question, answer) triples discarded due to char -> token mapping problems: ",
+          num_mappingprob)
+    print(
+        "Number of (context, question, answer) triples discarded because character-based answer span is unaligned with tokenization: ",
+        num_tokenprob)
+    print(
+        "Number of (context, question, answer) triples discarded due character span alignment problems (usually Unicode problems): ",
+        num_spanalignprob)
+    print("Processed %i examples of total %i\n" % (
+        num_exs, num_exs + num_mappingprob + num_tokenprob + num_spanalignprob))
 
     # shuffle examples
     indices = range(len(examples))
-    np.random.shuffle(indices)
+    np.random.shuffle(list(indices))
 
-    with open(os.path.join(out_dir, tier +'.context'), 'w') as context_file,  \
-         open(os.path.join(out_dir, tier +'.question'), 'w') as question_file,\
-         open(os.path.join(out_dir, tier +'.answer'), 'w') as ans_text_file, \
-         open(os.path.join(out_dir, tier +'.span'), 'w') as span_file:
+    with open(os.path.join(out_dir, tier + '.context'), 'wb') as context_file, \
+            open(os.path.join(out_dir, tier + '.question'), 'wb') as question_file, \
+            open(os.path.join(out_dir, tier + '.answer'), 'wb') as ans_text_file, \
+            open(os.path.join(out_dir, tier + '.span'), 'wb') as span_file:
 
         for i in indices:
             (context, question, answer, answer_span) = examples[i]
@@ -257,8 +265,8 @@ def preprocess_and_write(dataset, tier, out_dir):
 def main():
     args = setup_args()
 
-    print "Will download SQuAD datasets to {}".format(args.data_dir)
-    print "Will put preprocessed SQuAD datasets in {}".format(args.data_dir)
+    print("Will download SQuAD datasets to {}".format(args.data_dir))
+    print("Will put preprocessed SQuAD datasets in {}".format(args.data_dir))
 
     if not os.path.exists(args.data_dir):
         os.makedirs(args.data_dir)
@@ -267,21 +275,21 @@ def main():
     dev_filename = "dev-v1.1.json"
 
     # download train set
-    maybe_download(SQUAD_BASE_URL, train_filename, args.data_dir, 30288272L)
+    maybe_download(SQUAD_BASE_URL, train_filename, args.data_dir, 30288272)
 
     # read train set
     train_data = data_from_json(os.path.join(args.data_dir, train_filename))
-    print "Train data has %i examples total" % total_exs(train_data)
+    print("Train data has %i examples total" % total_exs(train_data))
 
     # preprocess train set and write to file
     preprocess_and_write(train_data, 'train', args.data_dir)
 
     # download dev set
-    maybe_download(SQUAD_BASE_URL, dev_filename, args.data_dir, 4854279L)
+    maybe_download(SQUAD_BASE_URL, dev_filename, args.data_dir, 4854279)
 
     # read dev set
     dev_data = data_from_json(os.path.join(args.data_dir, dev_filename))
-    print "Dev data has %i examples total" % total_exs(dev_data)
+    print("Dev data has %i examples total" % total_exs(dev_data))
 
     # preprocess dev set and write to file
     preprocess_and_write(dev_data, 'dev', args.data_dir)
--- get_started.sh
+++ get_started.sh
@@ -10,11 +10,11 @@ mkdir -p $EXP_DIR
 
 # Creates the environment
 #conda create -n squad python=2.7
-virtualenv -p /usr/bin/python2.7 squad
+#virtualenv -p /usr/bin/python2.7 squad
 
 # Activates the environment
 #source activate squad
-source squad/bin/activate
+#source squad/bin/activate
 
 # pip install into environment
 pip install -r requirements.txt
